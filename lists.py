fc52528b3f0dd6721091e2a1f603c61ed0c301f7
soyinkadgoat <soyinkadgreat807@gmail.com>
2026-07-11 13:01:43 +0100

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,12 @@
+empty_list = []
+print(empty_list)
+
+numbers_list = [1, 2, 3, 4, 5, 6]
+print(numbers_list)
+
+triples = numbers_list * 3
+print(triples)
+
+num_list = [10, 20, 30, 40, 50]
+reversed_list = num_list[::-1]
+print(reversed_list)
